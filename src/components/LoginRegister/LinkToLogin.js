914e9b7a3b102f57269b07694ffbe6246a009afe
utku <mehmetutkubalikci@gmail.com>
2024-09-24 08:10:56 +0300

    update call to login page

--- src/components/LoginRegister/LinkToLogin.js
+++ src/components/LoginRegister/LinkToLogin.js
@@ -1,16 +1,16 @@
 import { Button } from 'primereact/button';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Container } from 'reactstrap';
 
 export default function LinkToLogin() {
     const navigate = useNavigate();
     return (
-        <Container className='text-center flex justify-center m-4 bg-light'>
-            <p>Lütfen Giriş Yapın.</p>
-            <Button label="Giriş Yap" className="p-button-text" onClick={() => navigate('/login')} />
-
-        </Container>
+        <div className="surface-0 text-700 text-center mt-5">
+            <div className="text-900 font-bold text-5xl mb-3">Alışverişin Tadını Bizimle Çıkartın</div>
+            <div className="text-700 text-2xl mb-5">Giriş Yapın ya da Kayıt Olun</div>
+            <Button label="Giriş Yap" icon="pi pi-user" className="font-bold px-5 py-3 p-button-raised p-button-rounded white-space-nowrap" onClick={() => navigate('/login')} />
+            <Button label="Kayıt Ol" icon="pi pi-user-plus" severity="success" className="font-bold px-5 py-3 p-button-raised p-button-rounded white-space-nowrap" onClick={() => navigate('/register')} />
+        </div>
     )
 
 }
